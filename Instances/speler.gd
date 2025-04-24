6dae8685829ee7e94260d2b39e9607b34a8783dd
Quinten Eggermont <quinten.yimercraft@gmail.com>
2025-04-24 16:01:50 +0200

    startscherm

--- Instances/speler.gd
+++ Instances/speler.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+var previousPosition= Vector2(0,0)
 var currentPosition = Vector2(0,0)
 var stepSize = 64
 
@@ -7,18 +8,23 @@ func _input(event):
 	#als pijltjestoetsen
 	if event.is_action_pressed("ui_down"):
 		#player must move
+		previousPosition = currentPosition
 		currentPosition.y += stepSize
 	if event.is_action_pressed("ui_up"):
 		#player must move
+		previousPosition = currentPosition
 		currentPosition.y -= stepSize
 	if event.is_action_pressed("ui_left"):
 		#player must move
+		previousPosition = currentPosition
 		currentPosition.x -= stepSize
 	if event.is_action_pressed("ui_right"):
 		#player must move
+		previousPosition = currentPosition
 		currentPosition.x += stepSize
 	position = currentPosition
 
 
 func _on_area_2d_area_entered(area):
-	print("speler botst")
+	currentPosition = previousPosition
+	position = currentPosition
